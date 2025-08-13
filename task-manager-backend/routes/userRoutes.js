31c1626da562cd7f471addad7a75c8c58bf87004
[Mohd Shaan] <[mohdshaan321@gmail.com]>
2025-08-13 17:11:23 +0530

    updated file

--- task-manager-backend/routes/userRoutes.js
+++ task-manager-backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser, authUser} = require('../controllers/userController.jsx');
+const {registerUser, authUser} = require('../controllers/userController.js');
 router.post('/register', registerUser);
 router.post('/login', authUser);    
 module.exports = router;
